4fd1e80be82b7a721d5f05fda8ddfc8086ab72ea
James Labayna <32104185+araneidanGalanty@users.noreply.github.com>
2019-12-01 20:02:28 -0500

    Extremely basic form functionality working
    Ok. Now the product form "works," and the DOM adjusts based on user input.

--- products.js
+++ products.js
@@ -1,9 +1,40 @@
+const productNames = {
+  prod0: 'Proactiv',
+  prod1: 'Meaningful Beauty',
+  prod2: 'Pistaché Skincare',
+  prod3: 'Thayers',
+  prod4: 'Neutrogena',
+  prod5: 'Lubriderm',
+};
+
 $('#prod0').on('click', function (e) {
-  $('#overlay').show();
+  $('#overlay').fadeIn();
+  $('.buyFormTitle').text(productNames["prod0"]);
+})
+
+$('#prod1').on('click', function (e) {
+  $('#overlay').fadeIn();
+  $('.buyFormTitle').text(productNames["prod1"]);
+})
+$('#prod2').on('click', function (e) {
+  $('#overlay').fadeIn();
+  $('.buyFormTitle').text(productNames["prod2"]);
+})
+$('#prod3').on('click', function (e) {
+  $('#overlay').fadeIn();
+  $('.buyFormTitle').text(productNames["prod3"]);
+})
+$('#prod4').on('click', function (e) {
+  $('#overlay').fadeIn();
+  $('.buyFormTitle').text(productNames["prod4"]);
+})
+$('#prod5').on('click', function (e) {
+  $('#overlay').fadeIn();
+  $('.buyFormTitle').text(productNames["prod5"]);
 })
 
 $('#overlay').on('click', function (e) {
-  $('#overlay').hide();
+  $('#overlay').fadeOut();
 })
 
 $(".buyForm").click(function(e){
